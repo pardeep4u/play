3e228c705ec29f9d65dd7d812f9722e56e3ea7a0
sourav-vashisht <sourav.vashisht222@gmail.com>
2023-07-26 19:50:37 +0530

    this is just a begining

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [serviceList, setServiceList] = useState([{ service: "" }]);
+  const [outputList , setOutputList] = useState()
+
+  const handleServiceChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...serviceList];
+    list[index][name] = value;
+    setServiceList(list);
+  };
+
+  const handleServiceRemove = (index) => {
+    const list = [...serviceList];
+    list.splice(index, 1);
+    setServiceList(list);
+  };
+
+  const handleServiceAdd = () => {
+    setServiceList([...serviceList, { service: "" }]);
+  };
+
+  const handleOutput = (e) => {
+    e.preventDefault();
+    console.log(serviceList);
+
+
+
+
+  };
+
+  return (
+    <form className="App" autoComplete="off">
+      <div className="form-field">
+        <label htmlFor="service">City/Cities</label>
+        {serviceList.map((singleService, index) => (
+          <div key={index} className="services">
+            <div className="first-division">
+              <input
+                name="service"
+                type="text"
+                id="service"
+                value={singleService.service}
+                onChange={(e) => handleServiceChange(e, index)}
+                required
+              />
+              {serviceList.length - 1 === index && serviceList.length < 4 && (
+                <button
+                  type="button"
+                  onClick={handleServiceAdd}
+                  className="add-btn"
+                >
+                  <span>Add a City</span>
+                </button>
+              )}
+            </div>
+            <div className="second-division">
+              {serviceList.length !== 1 && (
+                <button
+                  type="button"
+                  onClick={() => handleServiceRemove(index)}
+                  className="remove-btn"
+                >
+                  <span>Remove</span>
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="output">
+        <h2>Output</h2>
+        <button onClick={handleOutput}>Click here to show result.</button>
+        { outputList.length !== 1 && }
+      </div>
+    </form>
+  );
+}
+
+export default App;
